c380ea30154d246e2dba21347bec3fe1278ce5c4
sbusho <sofiaabusho@gmail.com>
2024-10-10 14:17:46 +0300

    go-reloaded

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Checks if exactly two arguments are passed (input and output filenames)
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run main.go <input_file> <output_file>")
+		return
+	}
+
+	// Assign input and output filenames from command line arguments
+	inputFilename := os.Args[1]
+	outputFilename := os.Args[2]
+
+	// Read the content from the filename input
+	content, err := os.ReadFile(inputFilename)
+	if err != nil {
+		fmt.Println("Error reading the input file:", err)
+		return
+	}
+
+	// Apply modifications to the content
+	modifiedContent := string(content)
+
+	err = os.WriteFile(outputFilename, []byte(modifiedContent), 0o644)
+	if err != nil {
+		fmt.Println("Error writing to the output file:", err)
+		return
+	}
+
+	fmt.Println("Modifications applied successfully!")
+}
